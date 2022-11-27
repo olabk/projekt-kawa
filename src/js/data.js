7f847d9d122436c310065e7ab4fb376f4df5c7ab
olabk <olabaut@gmail.com>
2022-11-27 19:11:40 +0100

    add footer and header

---
+++ src/js/data.js
@@ -0,0 +1,37 @@
+const dataSource = {}; // eslint-disable-line no-unused-vars
+
+dataSource.products = {
+  lagartija: {
+    class: '',
+    name: '01. La Lagartija',
+    price: '',
+    description: 'Mysterious and treacherous... The best choice for the start of your journey! It will provide you a rich delicious flavor.',
+    images: [
+      '<img class="active" src="images/coffee-1.png">',
+    ],
+    roasting: '5/10',
+    intensity: '6/10',
+  },
+  tigre: {
+    class: '',
+    name: '02. El Tigre',
+    price: '',
+    description: 'Really dark coffee, only for the real gourmets. It has bitter flavor of grapefruits and is mixed with a little bit of peanuts with carmel.',
+    images: [
+      '<img class="active" src="images/coffee-2.png">',
+    ],
+    roasting: '8/10',
+    intensity: '9/10',
+  },
+  cucaracha: {
+    class: '',
+    name: '03. La Cucaracha',
+    price: '',
+    description: 'Sweet and intense with a big amount of different flavors. Caramel, grapes, strawberry, you name it! It is a greate choice for warm summer days',
+    images: [
+      '<img class="active" src="images/coffee-3.png">',
+    ],
+    roasting: '3/10',
+    intensity: '7/10',
+  },
+};
